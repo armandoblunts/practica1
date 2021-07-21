9fc208435a9a31df381f6def5209aee0cd92559d
Gallego <felipegallegomesa@gmail.com>
2021-07-21 16:53:43 -0500

    Initial commit

---
+++ conrolador.js
@@ -0,0 +1,10 @@
+//programa que nos ayude a establecer el costo total de una consola de video juegos
+
+//en el esterior
+//compra en dolares
+//pagar un casillero
+//(dolares costo dependiendo de la cantidad de productos)
+//envio hasta colombia
+//(cantidad de productos)
+//pagar impuesto de entrada al pais (lo digita aduanas)
+
